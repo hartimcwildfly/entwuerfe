53414abd912bf60dab8fd6f8447cc8579193275d
Matthias Harter <matthias.harter@quatico.com>
2021-10-18 15:32:47 +0200

    add initial sample spring project

---
+++ src/test/java/de/dhcd/entwuerfe/EntwuerfeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhcd.entwuerfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntwuerfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
